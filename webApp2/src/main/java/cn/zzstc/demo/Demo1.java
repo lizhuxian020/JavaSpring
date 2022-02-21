4ee398417e7d588cc725d63c499f2723be991e9e
lzx <lizhuxian@zzstc.cn>
2022-02-21 22:35:59 +0800

    asd\

---
+++ webApp2/src/main/java/cn/zzstc/demo/Demo1.java
@@ -0,0 +1,7 @@
+package cn.zzstc.demo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
--- webApp_demo/src/main/java/cn/zzstc/demo/UserDemo.java
+++ webApp_demo/src/main/java/cn/zzstc/demo/UserDemo.java
@@ -4,12 +4,15 @@ import cn.zzstc.dao.impl.UserDaoImple;
 import cn.zzstc.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class UserDemo {
     public static void main(String[] args) {
 
         ApplicationContext app = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        UserService userService = (UserService) app.getBean("userService");
+//        FileSystemXmlApplicationContext app = new FileSystemXmlApplicationContext("/Users/zhuxianli/workspace/JavaSpring/webApp_demo/src/main/resources/ApplicationContext.xml");
+//        UserService userService = (UserService) app.getBean("userService");
+        UserService userService = app.getBean(UserService.class);
         userService.save();
     }
 }
