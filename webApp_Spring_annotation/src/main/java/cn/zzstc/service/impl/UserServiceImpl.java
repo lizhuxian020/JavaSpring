7243cd05501dce7f4fd20729c2690868b7011f77
lizhuxian <lizhuxian@zzstc.cn>
2022-02-22 17:41:08 +0800

    asd

--- webApp_Spring_annotation/src/main/java/cn/zzstc/service/impl/UserServiceImpl.java
+++ webApp_Spring_annotation/src/main/java/cn/zzstc/service/impl/UserServiceImpl.java
@@ -4,15 +4,20 @@ import cn.zzstc.dao.UserDao;
 import cn.zzstc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 //<bean id="userService" class="cn.zzstc.service.impl.UserServiceImpl">
 //@Component("userService")
 @Service("userService")
 public class UserServiceImpl implements UserService {
+
+    @Value("${jdbc.driver}")
+    private String desc;
 
 //    <property name="userDao" ref="userDao"/>
 //    @Autowired //通过成员变量类型去spring寻找bean
@@ -22,6 +27,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save() {
+        System.out.println(this.desc);
         this.userDao.save();
     }
+
+    @PostConstruct
+    private void init() {
+        System.out.println("init running");
+    }
+
+
 }
