36dc9cd7b94c7b8370a572a1d0a50afb39cf845e
lizhuxian <lizhuxian@zzstc.cn>
2022-02-22 18:35:05 +0800

    asd

---
+++ webApp_Spring_annotation/src/main/java/cn/zzstc/config/SpringConfiguration.java
@@ -0,0 +1,42 @@
+package cn.zzstc.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+
+@Configuration
+@ComponentScan("cn.zzstc")
+//<context:property-placeholder location="classpath:props.properties"/>
+@PropertySource("classpath:props.properties")
+public class SpringConfiguration {
+
+    @Value("${jdbc.driver}")
+    public String driver;
+    @Value("${jdbc.url}")
+    public String url;
+    @Value("${jdbc.user}")
+    public String user;
+    @Value("${jdbc.pwd}")
+    public String pwd;
+
+    @Bean("dataSource")
+    public DataSource getDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+//        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/users");
+//        dataSource.setUser("root");
+//        dataSource.setPassword("qq123123");
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(pwd);
+        return dataSource;
+    }
+}
--- webApp_Spring_annotation/src/main/java/cn/zzstc/web/UserController.java
+++ webApp_Spring_annotation/src/main/java/cn/zzstc/web/UserController.java
@@ -1,13 +1,24 @@
 package cn.zzstc.web;
 
+import cn.zzstc.config.SpringConfiguration;
 import cn.zzstc.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class UserController {
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    public static void main(String[] args) throws SQLException {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         UserService userService = context.getBean(UserService.class);
         userService.save();
 
+        DataSource dataSource = context.getBean(DataSource.class);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 }
