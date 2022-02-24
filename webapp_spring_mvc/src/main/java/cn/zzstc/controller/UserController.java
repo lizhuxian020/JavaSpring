7bc335ce12fdc72cb8476ea8e1062a7840579359
lizhuxian <lizhuxian@zzstc.cn>
2022-02-24 11:35:04 +0800

    RequestMapping添加参数

--- webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
+++ webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
@@ -2,12 +2,13 @@ package cn.zzstc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/quick")
+    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
     private String func1() {
         System.out.println("asd");
         return "/success.jsp";
