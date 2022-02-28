9e98d900efe34cc5ffda48ca284d7d5373e2f5b0
lizhuxian <lizhuxian@zzstc.cn>
2022-02-28 10:29:11 +0800

    完成SpringMVC框架，获取Request和Response，数据回写

--- webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
+++ webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
@@ -3,15 +3,39 @@ package cn.zzstc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(value = "/quick", method = RequestMethod.GET, params = {"username"})
+    @RequestMapping("/quick4")
+    @ResponseBody
+    private String func4() {
+        return "asdasdasd";
+    }
+
+    @RequestMapping("/quick3")
+    private void func3(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setAttribute("username", "=.=");
+//        return "success";
+        response.getWriter().println("hahahha");
+    }
+
+    @RequestMapping("/quick2")
+    private String func2(HttpServletRequest request) {
+        request.setAttribute("username", "=.=");
+        return "success";
+    }
+
+    @RequestMapping(value = "/quick", method = RequestMethod.GET)
     private String func1() {
         System.out.println("asd");
-        return "/jsp/success.jsp";
+        return "redirect:/jsp/success.jsp";
     }
 
 }
--- webapp_spring_mvc/src/main/webapp/jsp/success.jsp
+++ webapp_spring_mvc/src/main/webapp/jsp/success.jsp
@@ -5,12 +5,12 @@
   Time: 4:57 PM
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-SUCCESS!!
+SUCCESS!!${username}
 </body>
 </html>
