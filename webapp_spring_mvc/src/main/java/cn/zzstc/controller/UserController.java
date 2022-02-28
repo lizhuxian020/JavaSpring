82ca2c545561444ef4d07f2cbce5de31405684fd
lizhuxian <lizhuxian@zzstc.cn>
2022-02-28 11:27:37 +0800

    配置HandlerAdapter，自动集成jackson库

--- webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
+++ webapp_spring_mvc/src/main/java/cn/zzstc/controller/UserController.java
@@ -1,8 +1,8 @@
 package cn.zzstc.controller;
 
 import cn.zzstc.domain.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,14 +18,23 @@ public class UserController {
 
     @RequestMapping("/quick5")
     @ResponseBody
-    private String func5() throws JsonProcessingException {
+    private User func5() {
         User user = new User();
-        user.setUsername("lisi");
-        user.setAge(123);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(user);
-        return json;
+        user.setUsername("l123isi");
+        user.setAge(1233);
+        return user;
     }
+
+//    @RequestMapping("/quick5")
+//    @ResponseBody
+//    private String func5() throws JsonProcessingException {
+//        User user = new User();
+//        user.setUsername("lisi");
+//        user.setAge(123);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String json = objectMapper.writeValueAsString(user);
+//        return json;
+//    }
 
     @RequestMapping("/quick4")
     @ResponseBody
