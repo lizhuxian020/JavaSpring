5d8f70e07d649a618d3402607c3e3a415bbee66c
lzx <lizhuxian@zzstc.cn>
2022-03-28 18:25:44 +0800

    asd

--- webApp_spring_jbdcTemplate/src/main/java/cn/zzstc/demo/Demo.java
+++ webApp_spring_jbdcTemplate/src/main/java/cn/zzstc/demo/Demo.java
@@ -1,20 +1,23 @@
 package cn.zzstc.demo;
 
+import cn.zzstc.domain.User;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Demo {
 
     public static void main(String[] args) throws PropertyVetoException, SQLException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
-        int row = jdbcTemplate.update("insert into user(name, phone) values (?,?)", "jack", "123321");
-        System.out.println(row);
+        List<User> userList = jdbcTemplate.query("select * from user", new BeanPropertyRowMapper<User>(User.class));
+        System.out.println(userList);
     }
 
     private void test1() throws PropertyVetoException {
@@ -30,6 +33,13 @@ public class Demo {
         jdbcTemplate.setDataSource(dataSource);
 
         int row = jdbcTemplate.update("insert into user(name, phone) values (?,?)", "tom", "3123123");
+        System.out.println(row);
+    }
+
+    private void test2() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        int row = jdbcTemplate.update("insert into user(name, phone) values (?,?)", "jack", "123321");
         System.out.println(row);
     }
 }
