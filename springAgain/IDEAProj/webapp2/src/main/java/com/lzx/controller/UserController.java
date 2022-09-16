951fc7f59fe5a65b5a9a52d412396f809dcd59eb
lzx <lizhuxian@zzstc.cn>
2022-09-16 23:01:38 +0800

    aasd

--- springAgain/IDEAProj/webapp2/src/main/java/com/lzx/controller/UserController.java
+++ springAgain/IDEAProj/webapp2/src/main/java/com/lzx/controller/UserController.java
@@ -1,12 +1,39 @@
 package com.lzx.controller;
 
+import com.lzx.domain.User;
 import com.lzx.service.UserService;
 import com.lzx.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/quick3")
+    @ResponseBody
+    public void quick3(String name, MultipartFile[] files) throws IOException {
+        System.out.println(name);
+        System.out.println(files);
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+            String fileName = file.getOriginalFilename();
+            file.transferTo(new File("/Users/zhuxianli/Desktop/" + i + fileName ));
+        }
+    }
+
+    @RequestMapping("/quick2")
+    @ResponseBody
+    public User quick2() {
+        User user = new User();
+        user.name = "asd";
+        user.age = 11;
+        return user;
+    }
 
     @RequestMapping("/quick1")
     public String quick1() {
---
+++ springAgain/IDEAProj/webapp2/src/main/java/com/lzx/domain/User.java
@@ -0,0 +1,6 @@
+package com.lzx.domain;
+
+public class User {
+    public String name;
+    public int age;
+}
