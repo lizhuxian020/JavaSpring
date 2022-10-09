d443e6b36edeb38d86efb3484aa15d76df1990af
lzx <lizhuxian@zzstc.cn>
2022-10-09 17:55:00 +0800

    动态sql标签

--- SpringMybatisDynamicProxy/src/main/java/com/lzm/dao/UserMapper.java
+++ SpringMybatisDynamicProxy/src/main/java/com/lzm/dao/UserMapper.java
@@ -7,4 +7,8 @@ import java.util.List;
 public interface UserMapper {
 
     public List<User> findAll();
+
+    public User findByCondition(User user);
+
+    public List<User> findByIds(int[] ids);
 }
--- SpringMybatisDynamicProxy/src/test/java/com/lzm/test/MybatisDemo.java
+++ SpringMybatisDynamicProxy/src/test/java/com/lzm/test/MybatisDemo.java
@@ -15,6 +15,34 @@ import java.util.List;
 public class MybatisDemo {
 
     @Test
+    public void test2() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("MybatisConfig.xml");
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = build.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int[] ids = {1, 2, 3};
+        List<User> all = mapper.findByIds(ids);
+        System.out.println(all);
+        sqlSession.close();;
+    }
+
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("MybatisConfig.xml");
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = build.openSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(1);
+        user.setName("lizx");
+        user.setPhone("321");
+
+        User byCondition = mapper.findByCondition(user);
+        System.out.println(byCondition);
+        sqlSession.close();;
+    }
+
+    @Test
     public void test() throws IOException {
         InputStream resourceAsStream = Resources.getResourceAsStream("MybatisConfig.xml");
         SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
