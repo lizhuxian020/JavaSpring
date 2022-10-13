49bca1845adc608e529ceb0fff3ce8c0dbe8ecb9
lzx <lizhuxian@zzstc.cn>
2022-10-13 14:44:29 +0800

    完结：完成spring_mybatis_transaction结合

--- SpringMybatisWebMVC/src/main/java/com/lzm/service/impl/AccountServiceImpl.java
+++ SpringMybatisWebMVC/src/main/java/com/lzm/service/impl/AccountServiceImpl.java
@@ -23,6 +23,7 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void save(Account account) {
        accountMapper.save(account);
+       int i = 1/0;
     }
 
     @Override
